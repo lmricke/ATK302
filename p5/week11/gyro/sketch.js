9194ff0366efbaac6975d4e726add62359fe047c
lmricke <lmricke@ilstu.edu>
2019-11-03 17:45:21 -0600

    weather

--- p5/week11/gyro/sketch.js
+++ p5/week11/gyro/sketch.js
@@ -4,18 +4,9 @@
 Make sure you turn on orientation lock on your iPhone or Android device. */
 
 
-
-
-
 var alpha, beta, gamma; // orientation data
-
-
 var bunnyImage;
-
-
 var xPosition = 0;
-
-
 var yPosition = 0;
 var x = 0; // acceleration data
 var y = 0;
@@ -38,15 +29,8 @@ function setup() {
 
 
   alpha = 0;
-
-
   beta = 0;
-
-
   gamma = 0;
-
-
-
 
 
   bunnyImage = loadImage("assets/bunny.jpg");
@@ -54,11 +38,7 @@ function setup() {
 
   imageMode(CENTER);
 
-
   rectMode(CENTER);
-
-
-
 
 
 }
@@ -69,51 +49,23 @@ function setup() {
 
 function draw() {
 
-
-
-
-
   background('#c6f5ff'); // light blue
-
-
-
 
 
   // the map command !!!!
 
-
   // takes your variable and maps it from range 1 to range 2
-
 
   // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
 
-
   xPosition = map(gamma, -60, 60, 0, width);
-
-
   yPosition = map(beta, -30, 30, 0, height);
-
-
-
-
 
   push(); // before you use translate, rotate, or scale commands, push and then pop after
 
-
-
-
-
   translate(xPosition, yPosition); // move everything over by x, y
 
-
-
-
-
   rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
-
-
-
-
 
   image(bunnyImage, 0, 0, 500, 500);
 
@@ -125,11 +77,6 @@ function draw() {
 
 
 
-
-
-
-
-
   // DECORATIONS
 
 
@@ -137,38 +84,16 @@ function draw() {
 
 
   textAlign(LEFT);
-
-
   textSize(20);
-
-
   fill('black');
-
-
   text("orientation data:", 25, 25);
-
-
   textSize(15);
-
-
   text("alpha: " + alpha, 25, 50);
-
-
   text("beta: " + beta, 25, 70);
-
-
   text("gamma: " + gamma, 25, 90);
-
-
   textSize(20);
-
-
   text("acceleration data:", 25, 125);
-
-
   textSize(15);
-
-
   //text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
 
 
@@ -185,22 +110,10 @@ function draw() {
 
 
   fill('white');
-
-
   noStroke();
-
-
   textSize(300);
-
-
   textAlign(CENTER);
-
-
   text("atk", width / 2, height / 2);
-
-
-
-
 
 }
 
@@ -219,23 +132,11 @@ function draw() {
 
 window.addEventListener('deviceorientation', function(e) {
 
-
   alpha = e.alpha;
-
-
   beta = e.beta;
-
-
   gamma = e.gamma;
 
-
 });
-
-
-
-
-
-
 
 
 // accelerometer Data
@@ -243,16 +144,11 @@ window.addEventListener('deviceorientation', function(e) {
 
 window.addEventListener('devicemotion', function(e) {
 
-
   // get accelerometer values
 
 
   x = e.acceleration.x;
-
-
   y = e.acceleration.y;
-
-
   z = e.acceleration.z;
 
 
